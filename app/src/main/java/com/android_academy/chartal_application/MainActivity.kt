197ada90874024334c75ae03353dcb145fe2861a
Artem <chartal@mail.ru>
2020-11-14 19:49:29 +0300

    feat: add layout for activity

--- app/src/main/java/com/android_academy/chartal_application/MainActivity.kt
+++ app/src/main/java/com/android_academy/chartal_application/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.android_academy.chartal_application
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textView: TextView = findViewById(R.id.tvHelloWorld)
+        textView.setOnClickListener {
+            movieToNextScreen()
+        }
+
     }
+
+    private fun movieToNextScreen() {
+        val intent = Intent(this, MovieDetailsActivity::class.java)
+        startActivity(intent)
+    }
+
+
 }
